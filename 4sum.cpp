ee7daff0ed7cf8c1121f4ad664a69133d53736d5
vAYUSH-114 <ayushvishwajr343@gmail.com>
2025-12-15 23:03:02 +0530

    Add files via upload

---
+++ 4sum.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, t;
+    cin >> n >> t;
+    vector<pair<long long, long long>> v(n);
+    for (long long i = 0; i < n; i++)
+    {
+        cin >> v[i].first;
+        v[i].second = i;
+    }
+
+    sort(v.begin(), v.end(), [](auto &a, auto &b)
+         { return a.first < b.first; });
+
+    for (long long i = 0; i < n; i++)
+    {
+        for (long long k = i + 1; k < n; k++)
+        {
+
+            long long t2 = t - v[i].first - v[k].first;
+            long long l = k + 1;
+            long long j = n - 1;
+            while (l < j)
+            {
+                long long sum = v[l].first + v[j].first;
+                if (sum == t2)
+                {
+                    cout << v[i].second + 1 << " " << v[k].second + 1 << " " << v[l].second + 1 << " " << v[j].second + 1 << endl;
+                    return 0;
+                }
+                else if (sum < t2)
+                {
+                    l++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE" << endl;
+
+    return 0;
+}
